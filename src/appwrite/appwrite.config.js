3b4671de65c0f604d27f5736c118f7272f51f1df
yongchang he <hyc0812@163.com>
2022-04-18 12:47:54 -0600

    set endpoint and ID

--- src/appwrite/appwrite.config.js
+++ src/appwrite/appwrite.config.js
@@ -1,2 +1,13 @@
 // Import the Appwrite module
 import { Appwrite } from "appwrite";
+
+// Init your Web SDK
+const appwrite = new Appwrite();
+
+appwrite
+    .setEndpoint('http://10.244.223.44/v1') // Your Appwrite Endpoint
+    .setProject('625da6a70473568b172a') // Your project ID
+;
+
+export const storage = appwrite.storage
+export const account = appwrite.account
